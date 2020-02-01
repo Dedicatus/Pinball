d769ab8b5483b332aefb197f8c7bbdc17a2ffa0a
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-02-01 00:46:52 -0800

    update prefabe

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    public float moveSpeed = 10f;
+    public float thrust = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(transform.forward * thrust);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
