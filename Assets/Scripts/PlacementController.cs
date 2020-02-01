b37ce1e49596618da62bc81e50be873ae99a86c1
Yiming Zhang <allenzym@gmail.com>
2020-02-01 01:09:09 -0800

    GridPlacement

---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementController : MonoBehaviour
+{
+    private Grid grid;
+
+    void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceNear(Vector3 clickPoint)
+    {
+        var finalPosiotion = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosiotion;
+    }
+}
