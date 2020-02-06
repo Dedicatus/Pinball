4a35ba05cdb2643f05bc85e379ce96eb50411333
Yiming Zhang <allenzym@gmail.com>
2020-02-06 00:38:04 -0800

    Enemy hitpoint

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int hitPoint = 3;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            --hitPoint;
+            if (hitPoint <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
