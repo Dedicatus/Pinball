3578bdd179c38293f88643b4536bbc81f5b5008a
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-02-05 23:14:45 -0800

    update ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -12,12 +12,32 @@ public class Ball : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        rigidBody.AddForce(transform.forward * thrust);
+        //rigidBody.AddForce(transform.forward * thrust);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Vector3 v = rigidBody.velocity;
+        //if(v.x*v.x+v.z*v.z< moveSpeed*moveSpeed)
+        //rigidBody.AddForce(transform.forward * thrust);
+
+        //rigidBody.velocity = new Vector3(1f,0,1f) * moveSpeed;
+        rigidBody.MovePosition(transform.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            
+            ContactPoint contactPoint = collision.GetContact(0);
+            Vector3 newDir = Vector3.zero;
+            Vector3 curDir = transform.TransformDirection(Vector3.forward);
+            newDir = Vector3.Reflect(curDir, contactPoint.normal);
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, newDir);
+            Debug.Log(newDir.x+"   "+ newDir.z);
+            transform.rotation = rotation;
+        }
     }
 }
