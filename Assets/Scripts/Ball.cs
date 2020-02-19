07acbb6b7aeadf1c66df7f5c5ee3ce38c5cc2b2d
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-02-18 16:32:42 -0800

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,15 +8,18 @@ public class Ball : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float thrust = 10f;
+    public int MaxBounce = 3;
     // Start is called before the first frame update
     private GameObject closestTower;
     private GameObject[] allTowers;
+    private float BounceCount;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         //rigidBody.AddForce(transform.forward * thrust);
         transform.position = new Vector3(transform.position.x, 0.16f, transform.position.z);
         rigidBody.constraints = RigidbodyConstraints.FreezePositionY;
+        BounceCount = 0;
     }
 
     // Update is called once per frame
@@ -37,6 +40,7 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.tag == "Tower")
         {
+            BounceCount = 0;
             ContactPoint contactPoint = collision.GetContact(0);
             Vector3 newDir = Vector3.zero;
             Vector3 curDir = transform.TransformDirection(Vector3.forward);
@@ -44,10 +48,31 @@ public class Ball : MonoBehaviour
             Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, newDir);
             
             //Debug.Log(newDir.x+"   "+ newDir.z);
-            transform.rotation = Quaternion.Euler(rotation.x, rotation.y+ Random.Range(-30f, 30f), rotation.z);
+            transform.rotation = rotation;
         }
         if (collision.gameObject.tag == "Wall")
+        {
+            BounceCount++;
+            if (BounceCount < MaxBounce)
+            {
+                
+                ContactPoint contactPoint = collision.GetContact(0);
+                Vector3 newDir = Vector3.zero;
+                Vector3 curDir = transform.TransformDirection(Vector3.forward);
+                newDir = Vector3.Reflect(curDir, contactPoint.normal);
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, newDir);
+                //Debug.Log(newDir.x+"   "+ newDir.z);
+                transform.rotation = rotation;
+                Debug.Log("YES");
+            }
+            else
+            {
+                BounceCount = 0;
                 FindClosestTower();
+            }
+            
+        }
+                
     }
 
     void FindClosestTower()
